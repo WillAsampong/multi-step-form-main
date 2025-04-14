151c58f246ea9ad1545d38cc4fb6f6cd5469432f
Will <asampongg377@gmail.com>
2025-04-14 19:14:22 +0000

    Init

--- multi-step-form-project/src/Components/Sidebar.jsx
+++ multi-step-form-project/src/Components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Sidebar = ({ steps, currentStep }) => (
-    <div className="flex md:flex-col gap-6 pt-10 md:p-6 items-start justify-center bg-[url('./assets/images/bg-sidebar-mobile.svg')] md:bg-[url('./assets/images/bg-sidebar-desktop.svg')] bg-no-repeat bg-center md:h-auto h-[150px] bg-cover md:rounded-md md:w-1/5">
+    <div className="flex md:flex-col gap-6 pt-10 md:p-6 items-start justify-center md:justify-start bg-[url('./assets/images/bg-sidebar-mobile.svg')] md:bg-[url('./assets/images/bg-sidebar-desktop.svg')] bg-no-repeat bg-center md:h-auto h-[150px] bg-cover md:rounded-md md:w-1/5">
       {steps.map((item) => (
         <div className='flex gap-x-4' key={item.id}>
           <div 
